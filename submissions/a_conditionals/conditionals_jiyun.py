f637c3091a820c968801a4a117a7c960c0265d1d
JiyunLee2021 <jiyunvia@gmail.com>
2021-09-28 17:58:01 +0200

    Today

--- submissions/a_conditionals/conditionals_jiyun.py
+++ submissions/a_conditionals/conditionals_jiyun.py
@@ -35,6 +35,12 @@ city_2_unemployment_rate = 7.4
 city_3_name = 'Amsterdam'
 city_3_population = 2431000
 city_3_unemployment_rate = 3.3
+city_3_name = 'Bogota'
+city_3_population = 2431000
+city_3_unemployment_rate = 3.3
+city_4_name = 'Seoul'
+city_4_population = 9431000
+city_4_unemployment_rate = 23.3
 
 # note: Mind the indented structure of if clauses. It matters!!!!
 
@@ -75,14 +81,14 @@ print(result)
 
 
 # if, if elif and if else classes can be nested. Indentation plays the main role here
-if city_1_population > city_2_population:
-    if city_1_unemployment_rate > city_2_unemployment_rate:
-        result = city_1_name + ' is bigger and has a higher unemployment rate than ' + city_2_name
+if city_4_population > city_2_population:
+    if city_4_unemployment_rate > city_2_unemployment_rate:
+        result = city_4_name + ' is bigger and has a higher unemployment rate than ' + city_2_name
     else:
-        result = city_1_name + ' is bigger but has a lower unemployment rate than ' + city_2_name
+        result = city_4_name + ' is bigger but has a lower unemployment rate than ' + city_2_name
 else:
-    if city_1_unemployment_rate > city_2_unemployment_rate:
-        result = city_2_name + ' is bigger and has a higher unemployment rate than ' + city_1_name
+    if city_4_unemployment_rate > city_2_unemployment_rate:
+        result = city_2_name + ' is bigger and has a higher unemployment rate than ' + city_4_name
     else:
-        result = city_2_name + ' is bigger but has a lower unemployment rate than ' + city_1_name
+        result = city_2_name + ' is bigger but has a lower unemployment rate than ' + city_4_name
 print(result)
